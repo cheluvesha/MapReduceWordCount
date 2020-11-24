a49f9a04f5df18c8d09402ad32c517aebe48586f
cheluvesha <cheluvesh.b@gmail.com>
2020-11-24 13:33:38 +0530

    [CHELUVESHA]Added: Exception and Logger

--- src/main/scala/com/WordCount/WordCountReducer.scala
+++ src/main/scala/com/WordCount/WordCountReducer.scala
@@ -2,14 +2,27 @@ package com.WordCount
 
 import java.io.IOException
 import java.util
+import java.util.logging.Logger
 
 import org.apache.hadoop.io.{IntWritable, Text}
 import org.apache.hadoop.mapred.{MapReduceBase, OutputCollector, Reducer, Reporter}
 
+/***
+ * WordCountReducer Which Implements Reduce Task
+ */
 class WordCountReducer
     extends MapReduceBase
     with Reducer[Text, IntWritable, Text, IntWritable] {
+    val logger: Logger = Logger.getLogger(getClass.getName)
 
+  /***
+   * Reduce Function Implements Reduce task
+   * @param key Text
+   * @param values util.Iterator[IntWritable]
+   * @param output OutputCollector[Text, IntWritable]
+   * @param reporter Reporter
+   * @throws IOException
+   */
   @throws[IOException]
   def reduce(
       key: Text,
@@ -17,11 +30,18 @@ class WordCountReducer
       output: OutputCollector[Text, IntWritable],
       reporter: Reporter
   ) {
+    logger.info("Inside Reduce")
+    try {
     import scala.collection.JavaConversions._
     val sum = values.toList.reduce((valueOne, valueTwo) =>
       new IntWritable(valueOne.get() + valueTwo.get())
     )
-
+    logger.info(key+"   ->  "+ sum )
     output.collect(key, new IntWritable(sum.get()))
+  }
+    catch {
+      case ex: Exception =>
+        println(ex.getMessage)
+    }
   }
 }
