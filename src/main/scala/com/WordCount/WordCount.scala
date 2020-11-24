963ef7d2d9297d592c0dec70d4084ce563d1b6eb
cheluvesha <cheluvesh.b@gmail.com>
2020-11-24 13:32:54 +0530

    [CHELUVESHA]Added: Exception

--- src/main/scala/com/WordCount/WordCount.scala
+++ src/main/scala/com/WordCount/WordCount.scala
@@ -1,12 +1,21 @@
 package com.WordCount
 
+/***
+ * Dependencies Used Hadoop Client check build.sbt for More
+ * Plugins Sbt-Scapegoat for Code Analysis
+ */
+
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.{IntWritable, Text}
 import org.apache.hadoop.mapred._
 
+/***
+ * WordCount which Creates Configuration and Jobs
+ */
 object WordCount {
   @throws[Exception]
   def main(args: Array[String]) {
+    try {
     val conf: JobConf = new JobConf(this.getClass)
     conf.setJobName("WordCountScala")
     conf.setOutputKeyClass(classOf[Text])
@@ -19,5 +28,10 @@ object WordCount {
     FileInputFormat.setInputPaths(conf, new Path("input"))
     FileOutputFormat.setOutputPath(conf, new Path("output"))
     JobClient.runJob(conf)
+  }
+    catch {
+      case ex: Exception =>
+        println(ex.getMessage)
+    }
   }
 }
